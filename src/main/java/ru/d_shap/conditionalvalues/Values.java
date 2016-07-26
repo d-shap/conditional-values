46712cb052826678e950cd928c538fae6ce4d9de
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-07-26 07:54:52 +0300

    добавил javadoc

--- src/main/java/ru/d_shap/conditionalvalues/Values.java
+++ src/main/java/ru/d_shap/conditionalvalues/Values.java
@@ -10,6 +10,12 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Represents defined in runtime values;
+ *
+ * @param <T> Value type.
+ * @author Dmitry Shapovalov
+ */
 public final class Values<T> {
 
     private final List<ValueSet<T>> _valueSets;
@@ -26,14 +32,30 @@ public final class Values<T> {
         _allValues = Collections.unmodifiableSet(allValues);
     }
 
+    /**
+     * Does this object contains any values.
+     *
+     * @return true if does not.
+     */
     public boolean isEmpty() {
         return _allValues.isEmpty();
     }
 
+    /**
+     * Does this object contains specified value.
+     *
+     * @param value test value.
+     * @return true if contains.
+     */
     public boolean contains(final T value) {
         return _allValues.contains(value);
     }
 
+    /**
+     * Return all values contained in this object.
+     *
+     * @return all values.
+     */
     public Set<T> getAllValues() {
         return _allValues;
     }
