3f6df8448ec74dc2fe3a85bc05968ebeaf88e336
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-07-26 07:30:51 +0300

    убрал дублирующий файл

--- src/test/java/ru/d_shap/conditionalvalues/package-info.java
+++
@@ -1,8 +0,0 @@
-// //////////////////////////////
-// This code is authored
-// Dmitry Shapovalov
-// //////////////////////////////
-/**
- * Package for conditional values.
- */
-package ru.d_shap.conditionalvalues;
