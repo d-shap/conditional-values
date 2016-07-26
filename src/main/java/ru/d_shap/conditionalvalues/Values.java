c225c1387545dffa47c08a1c1b02d4d0b089050e
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-07-26 07:55:15 +0300

    добавил javadoc

--- src/main/java/ru/d_shap/conditionalvalues/Values.java
+++ src/main/java/ru/d_shap/conditionalvalues/Values.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * Represents defined in runtime values;
+ * Represents defined in runtime values.
  *
  * @param <T> Value type.
  * @author Dmitry Shapovalov
