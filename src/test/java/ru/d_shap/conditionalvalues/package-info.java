fdc63f4ff5829aab58602354a551c215d6bbdf8c
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-08-01 07:18:48 +0300

    добавил package comments для тестов

---
+++ src/test/java/ru/d_shap/conditionalvalues/package-info.java
@@ -0,0 +1,8 @@
+// //////////////////////////////
+// This code is authored
+// Dmitry Shapovalov
+// //////////////////////////////
+/**
+ * Tests for conditional values classes.
+ */
+package ru.d_shap.conditionalvalues;
