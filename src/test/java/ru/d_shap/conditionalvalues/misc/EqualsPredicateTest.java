737249e54f083f25e16b8aef15bd78411690f311
Dmitry Shapovalov <shenf@mail.ru>
2019-06-06 11:57:36 +0300

    Реализовал тесты на работу предиката

--- src/test/java/ru/d_shap/conditionalvalues/misc/EqualsPredicateTest.java
+++ src/test/java/ru/d_shap/conditionalvalues/misc/EqualsPredicateTest.java
@@ -21,6 +21,8 @@ package ru.d_shap.conditionalvalues.misc;
 
 import org.junit.Test;
 
+import ru.d_shap.assertions.Assertions;
+
 /**
  * Tests for {@link EqualsPredicate}.
  *
@@ -40,7 +42,26 @@ public final class EqualsPredicateTest {
      */
     @Test
     public void evaluateTest() {
-        // TODO
+        Assertions.assertThat(new EqualsPredicate().evaluate(null, null, null)).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate(null, "value", null)).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate(null, null, "value")).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate(null, "value1", "value2")).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate(null, "value", "value")).isTrue();
+        Assertions.assertThat(new EqualsPredicate().evaluate(null, "vaLUe", "ValUE")).isFalse();
+
+        Assertions.assertThat(new EqualsPredicate().evaluate("", null, null)).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate("", "value", null)).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate("", null, "value")).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate("", "value1", "value2")).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate("", "value", "value")).isTrue();
+        Assertions.assertThat(new EqualsPredicate().evaluate("", "vaLUe", "ValUE")).isFalse();
+
+        Assertions.assertThat(new EqualsPredicate().evaluate("condition", null, null)).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate("condition", "value", null)).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate("condition", null, "value")).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate("condition", "value1", "value2")).isFalse();
+        Assertions.assertThat(new EqualsPredicate().evaluate("condition", "value", "value")).isTrue();
+        Assertions.assertThat(new EqualsPredicate().evaluate("condition", "vaLUe", "ValUE")).isFalse();
     }
 
 }
