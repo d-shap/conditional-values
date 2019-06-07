b6c6a4f96e33a421e1c02c853cae7c032fb9777f
Dmitry Shapovalov <shenf@mail.ru>
2019-06-07 15:22:05 +0300

    Добавил дополнительные проверки на null при создании объектов

--- src/test/java/ru/d_shap/conditionalvalues/ConditionSetTest.java
+++ src/test/java/ru/d_shap/conditionalvalues/ConditionSetTest.java
@@ -45,21 +45,37 @@ public final class ConditionSetTest {
      */
     @Test
     public void nameIteratorTest() {
-        Map<String, String> conditions1 = new HashMap<>();
+        Map<String, String> conditions1 = null;
         ConditionSet conditionSet1 = new ConditionSet(conditions1);
         Assertions.assertThat(conditionSet1.nameIterator()).containsExactly();
 
         Map<String, String> conditions2 = new HashMap<>();
-        conditions2.put("cond1", "val1");
         ConditionSet conditionSet2 = new ConditionSet(conditions2);
-        Assertions.assertThat(conditionSet2.nameIterator()).containsExactly("cond1");
+        Assertions.assertThat(conditionSet2.nameIterator()).containsExactly();
 
         Map<String, String> conditions3 = new HashMap<>();
         conditions3.put("cond1", "val1");
-        conditions3.put("cond2", "val2");
-        conditions3.put("cond3", "val3");
         ConditionSet conditionSet3 = new ConditionSet(conditions3);
-        Assertions.assertThat(conditionSet3.nameIterator()).containsExactly("cond1", "cond2", "cond3");
+        Assertions.assertThat(conditionSet3.nameIterator()).containsExactly("cond1");
+
+        Map<String, String> conditions4 = new HashMap<>();
+        conditions4.put("cond1", "val1");
+        conditions4.put("cond2", null);
+        conditions4.put(null, "val2");
+        ConditionSet conditionSet4 = new ConditionSet(conditions4);
+        Assertions.assertThat(conditionSet4.nameIterator()).containsExactly("cond1");
+
+        Map<String, String> conditions5 = new HashMap<>();
+        conditions5.put(null, null);
+        ConditionSet conditionSet5 = new ConditionSet(conditions5);
+        Assertions.assertThat(conditionSet5.nameIterator()).containsExactly();
+
+        Map<String, String> conditions6 = new HashMap<>();
+        conditions6.put("cond1", "val1");
+        conditions6.put("cond2", "val2");
+        conditions6.put("cond3", "val3");
+        ConditionSet conditionSet6 = new ConditionSet(conditions6);
+        Assertions.assertThat(conditionSet6.nameIterator()).containsExactly("cond1", "cond2", "cond3");
     }
 
     /**
@@ -67,7 +83,7 @@ public final class ConditionSetTest {
      */
     @Test
     public void getValueTest() {
-        Map<String, String> conditions1 = new HashMap<>();
+        Map<String, String> conditions1 = null;
         ConditionSet conditionSet1 = new ConditionSet(conditions1);
         Assertions.assertThat(conditionSet1.getValue("cond1")).isNull();
         Assertions.assertThat(conditionSet1.getValue("cond2")).isNull();
@@ -75,22 +91,47 @@ public final class ConditionSetTest {
         Assertions.assertThat(conditionSet1.getValue("cond4")).isNull();
 
         Map<String, String> conditions2 = new HashMap<>();
-        conditions2.put("cond1", "val1");
         ConditionSet conditionSet2 = new ConditionSet(conditions2);
-        Assertions.assertThat(conditionSet2.getValue("cond1")).isEqualTo("val1");
+        Assertions.assertThat(conditionSet2.getValue("cond1")).isNull();
         Assertions.assertThat(conditionSet2.getValue("cond2")).isNull();
         Assertions.assertThat(conditionSet2.getValue("cond3")).isNull();
         Assertions.assertThat(conditionSet2.getValue("cond4")).isNull();
 
         Map<String, String> conditions3 = new HashMap<>();
         conditions3.put("cond1", "val1");
-        conditions3.put("cond2", "val2");
-        conditions3.put("cond3", "val3");
         ConditionSet conditionSet3 = new ConditionSet(conditions3);
         Assertions.assertThat(conditionSet3.getValue("cond1")).isEqualTo("val1");
-        Assertions.assertThat(conditionSet3.getValue("cond2")).isEqualTo("val2");
-        Assertions.assertThat(conditionSet3.getValue("cond3")).isEqualTo("val3");
+        Assertions.assertThat(conditionSet3.getValue("cond2")).isNull();
+        Assertions.assertThat(conditionSet3.getValue("cond3")).isNull();
         Assertions.assertThat(conditionSet3.getValue("cond4")).isNull();
+
+        Map<String, String> conditions4 = new HashMap<>();
+        conditions4.put("cond1", "val1");
+        conditions4.put("cond2", null);
+        conditions4.put(null, "val2");
+        ConditionSet conditionSet4 = new ConditionSet(conditions4);
+        Assertions.assertThat(conditionSet4.getValue("cond1")).isEqualTo("val1");
+        Assertions.assertThat(conditionSet4.getValue("cond2")).isNull();
+        Assertions.assertThat(conditionSet4.getValue("cond3")).isNull();
+        Assertions.assertThat(conditionSet4.getValue("cond4")).isNull();
+
+        Map<String, String> conditions5 = new HashMap<>();
+        conditions5.put(null, null);
+        ConditionSet conditionSet5 = new ConditionSet(conditions5);
+        Assertions.assertThat(conditionSet5.getValue("cond1")).isNull();
+        Assertions.assertThat(conditionSet5.getValue("cond2")).isNull();
+        Assertions.assertThat(conditionSet5.getValue("cond3")).isNull();
+        Assertions.assertThat(conditionSet5.getValue("cond4")).isNull();
+
+        Map<String, String> conditions6 = new HashMap<>();
+        conditions6.put("cond1", "val1");
+        conditions6.put("cond2", "val2");
+        conditions6.put("cond3", "val3");
+        ConditionSet conditionSet6 = new ConditionSet(conditions6);
+        Assertions.assertThat(conditionSet6.getValue("cond1")).isEqualTo("val1");
+        Assertions.assertThat(conditionSet6.getValue("cond2")).isEqualTo("val2");
+        Assertions.assertThat(conditionSet6.getValue("cond3")).isEqualTo("val3");
+        Assertions.assertThat(conditionSet6.getValue("cond4")).isNull();
     }
 
     /**
@@ -98,23 +139,39 @@ public final class ConditionSetTest {
      */
     @Test
     public void toStringTest() {
-        Map<String, String> conditions1 = new HashMap<>();
+        Map<String, String> conditions1 = null;
         ConditionSet conditionSet1 = new ConditionSet(conditions1);
         Assertions.assertThat(conditionSet1).hasToString("{}");
 
         Map<String, String> conditions2 = new HashMap<>();
-        conditions2.put("cond1", "val1");
         ConditionSet conditionSet2 = new ConditionSet(conditions2);
-        Assertions.assertThat(conditionSet2).hasToString("{cond1=val1}");
+        Assertions.assertThat(conditionSet2).hasToString("{}");
 
         Map<String, String> conditions3 = new HashMap<>();
         conditions3.put("cond1", "val1");
-        conditions3.put("cond2", "val2");
-        conditions3.put("cond3", "val3");
         ConditionSet conditionSet3 = new ConditionSet(conditions3);
-        Assertions.assertThat(conditionSet3).toStringContains("cond1=val1");
-        Assertions.assertThat(conditionSet3).toStringContains("cond2=val2");
-        Assertions.assertThat(conditionSet3).toStringContains("cond3=val3");
+        Assertions.assertThat(conditionSet3).hasToString("{cond1=val1}");
+
+        Map<String, String> conditions4 = new HashMap<>();
+        conditions4.put("cond1", "val1");
+        conditions4.put("cond2", null);
+        conditions4.put(null, "val2");
+        ConditionSet conditionSet4 = new ConditionSet(conditions4);
+        Assertions.assertThat(conditionSet4).hasToString("{cond1=val1}");
+
+        Map<String, String> conditions5 = new HashMap<>();
+        conditions5.put(null, null);
+        ConditionSet conditionSet5 = new ConditionSet(conditions5);
+        Assertions.assertThat(conditionSet5).hasToString("{}");
+
+        Map<String, String> conditions6 = new HashMap<>();
+        conditions6.put("cond1", "val1");
+        conditions6.put("cond2", "val2");
+        conditions6.put("cond3", "val3");
+        ConditionSet conditionSet6 = new ConditionSet(conditions6);
+        Assertions.assertThat(conditionSet6).toStringContains("cond1=val1");
+        Assertions.assertThat(conditionSet6).toStringContains("cond2=val2");
+        Assertions.assertThat(conditionSet6).toStringContains("cond3=val3");
     }
 
 }
