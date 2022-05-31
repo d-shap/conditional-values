cbd852c32ea7ffe6686f7698dfc44593c587c0dc
Dmitry Shapovalov <shenf@mail.ru>
2022-05-31 14:58:00 +0300

    Класс ошибки

---
+++ src/main/java/ru/d_shap/conditionalvalues/predicate/WrongValueSetValueException.java
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Conditional values simplify conditional logic and get rid of if-statements in the code.
+// Copyright (C) 2016 Dmitry Shapovalov.
+//
+// This file is part of conditional values.
+//
+// Conditional values is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Conditional values is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.conditionalvalues.predicate;
+
+/**
+ * Exception is thrown when the value from the {@link ru.d_shap.conditionalvalues.ValueSet} object is wrong.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class WrongValueSetValueException extends EvaluationException {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Create new object.
+     *
+     * @param conditionName the name of condition.
+     * @param valueSetValue the value from the {@link ru.d_shap.conditionalvalues.ValueSet} object.
+     * @param expectedClass the expected class for the value from the {@link ru.d_shap.conditionalvalues.ValueSet} object.
+     */
+    public WrongValueSetValueException(final String conditionName, final Object valueSetValue, final Class<?> expectedClass) {
+        super(getMessage(conditionName, getValueSetValueClass(valueSetValue), expectedClass));
+    }
+
+    private static String getMessage(final String conditionName, final Class<?> valueSetValueClass, final Class<?> expectedClass) {
+        return "Condition with name " + conditionName + " has a wrong class, expected " + expectedClass + ", but was " + valueSetValueClass;
+    }
+
+    private static Class<?> getValueSetValueClass(final Object valueSetValue) {
+        if (valueSetValue == null) {
+            return null;
+        } else {
+            return valueSetValue.getClass();
+        }
+    }
+
+}
