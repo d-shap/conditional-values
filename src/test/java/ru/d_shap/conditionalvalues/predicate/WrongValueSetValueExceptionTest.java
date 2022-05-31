508aa6420470c56afd5b438e6ba232f58e8915e5
Dmitry Shapovalov <shenf@mail.ru>
2022-05-31 15:04:44 +0300

    Добавил тесты

---
+++ src/test/java/ru/d_shap/conditionalvalues/predicate/WrongValueSetValueExceptionTest.java
@@ -0,0 +1,60 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Conditional values simplify conditional logic and get rid of if-statements in the code.
+// Copyright (C) 2016 Dmitry Shapovalov.
+//
+// This file is part of conditional values.
+//
+// Conditional values is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Conditional values is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.conditionalvalues.predicate;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.Assertions;
+
+/**
+ * Tests for {@link WrongValueSetValueException}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class WrongValueSetValueExceptionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public WrongValueSetValueExceptionTest() {
+        super();
+    }
+
+    /**
+     * {@link WrongValueSetValueException} class test.
+     */
+    @Test
+    public void getMessageTest() {
+        Assertions.assertThat(new WrongValueSetValueException(null, null, null)).hasMessage("Condition with name null has a wrong class, expected null, but was null");
+        Assertions.assertThat(new WrongValueSetValueException("condition", "value", StringBuilder.class)).hasMessage("Condition with name condition has a wrong class, expected class java.lang.StringBuilder, but was class java.lang.String");
+        Assertions.assertThat(new WrongValueSetValueException("cnd", 11, Double.class)).hasMessage("Condition with name cnd has a wrong class, expected class java.lang.Double, but was class java.lang.Integer");
+    }
+
+    /**
+     * {@link WrongValueSetValueException} class test.
+     */
+    @Test
+    public void getCauseTest() {
+        Assertions.assertThat(new WrongValueSetValueException(null, null, null)).causeIsNull();
+        Assertions.assertThat(new WrongValueSetValueException("condition", "value", StringBuilder.class)).causeIsNull();
+        Assertions.assertThat(new WrongValueSetValueException("cnd", 11, Double.class)).causeIsNull();
+    }
+
+}
