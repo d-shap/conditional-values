8f7505b04f537c3cf0aa79e58759285a92b875f4
Dmitry Shapovalov <shenf@mail.ru>
2022-06-03 23:41:10 +0300

    Реализовал проверки предиката

--- src/test/java/ru/d_shap/conditionalvalues/predicate/LogicalNotPredicateTest.java
+++ src/test/java/ru/d_shap/conditionalvalues/predicate/LogicalNotPredicateTest.java
@@ -21,6 +21,8 @@ package ru.d_shap.conditionalvalues.predicate;
 
 import org.junit.Test;
 
+import ru.d_shap.assertions.Assertions;
+
 /**
  * Tests for {@link LogicalNotPredicate}.
  *
@@ -40,7 +42,45 @@ public final class LogicalNotPredicateTest {
      */
     @Test
     public void evaluateTest() {
-        // TODO
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate(null, null, null)).isFalse();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate(null, "value", null)).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate(null, null, "value")).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate(null, "value1", "value2")).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate(null, "value", "value")).isFalse();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate(null, "vaLUe", "ValUE")).isTrue();
+
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("", null, null)).isFalse();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("", "value", null)).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("", null, "value")).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("", "value1", "value2")).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("", "value", "value")).isFalse();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("", "vaLUe", "ValUE")).isTrue();
+
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("condition", null, null)).isFalse();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("condition", "value", null)).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("condition", null, "value")).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("condition", "value1", "value2")).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("condition", "value", "value")).isFalse();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("condition", "vaLUe", "ValUE")).isTrue();
+
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", null, null)).isFalse();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", 5, null)).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", null, 5)).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", 5, 5)).isFalse();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", 5, 6)).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", 6, 5)).isTrue();
+
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", "value", "value")).isFalse();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", new StringBuilder("value"), "value")).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", "value", new StringBuilder("value"))).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", new StringBuilder("value"), new StringBuilder("value"))).isTrue();
+
+        Object obj1 = new StringBuilder("value");
+        Object obj2 = new Object();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", obj1, obj1)).isFalse();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", obj2, obj2)).isFalse();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", obj1, obj2)).isTrue();
+        Assertions.assertThat(new LogicalNotPredicate(new EqualsPredicate()).evaluate("c", obj2, obj1)).isTrue();
     }
 
 }
