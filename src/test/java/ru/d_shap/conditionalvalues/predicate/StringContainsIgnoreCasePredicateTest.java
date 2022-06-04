127a4e12bc8abbc8329e6502bfff3e88d5710c36
Dmitry Shapovalov <shenf@mail.ru>
2022-06-04 17:11:13 +0300

    Заглушки для тестов

---
+++ src/test/java/ru/d_shap/conditionalvalues/predicate/StringContainsIgnoreCasePredicateTest.java
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Conditional values simplify conditional logic and get rid of if-statements in the code.
+// Copyright (C) 2016 Dmitry Shapovalov.
+//
+// This file is part of conditional values.
+//
+// Conditional values is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Conditional values is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.conditionalvalues.predicate;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link StringContainsIgnoreCasePredicate}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class StringContainsIgnoreCasePredicateTest {
+
+    /**
+     * Test class constructor.
+     */
+    public StringContainsIgnoreCasePredicateTest() {
+        super();
+    }
+
+    /**
+     * {@link StringContainsIgnoreCasePredicate} class test.
+     */
+    @Test
+    public void evaluateTest() {
+        // TODO
+    }
+
+    /**
+     * {@link StringContainsIgnoreCasePredicate} class test.
+     */
+    @Test
+    public void lookupTest() {
+        // TODO
+    }
+
+}
